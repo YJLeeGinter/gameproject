926fa901b3358b6857b43f2f37cc2d9ebc356bce
Yeon Jung Lee <ginterlee29@gmail.com>
2019-07-11 01:01:30 -0400

    modified the code

--- jsgame.js
+++ jsgame.js
@@ -1,3 +1,12 @@
-document.querySelector('.button').addEventListener('click', function () {
-    alert('Clicked!');
-  });
+const startButton = document.querySelector('button');
+startButton.addEventListener('click', colorChange);
+
+function colorChange(){
+    const elems = document.getElementsByClassName('first');
+    for(let i =0; i < 3; i++){
+        elems[i].className = 'second';
+    }
+  for(let i =4; i < 7; i++){
+        elems[i].className = 'third';
+    }
+}
